f1428e24e6bf795d2f0c4e66d025e7dd8db79174
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-02 11:31:40 +0000

    Name Links Added

---
+++ src/data/Footer/Services.ts
@@ -0,0 +1,21 @@
+type Type = {
+    Title: string,
+    name1: string,
+    name2: string,
+    name3?: string,
+    name4?: string,
+    name5?: string,
+    name6?: string,
+    name7?: string,
+    url1: string,
+    url2: string,
+    url3?: string,
+    url4?: string,
+    url5?: string,
+    url6?: string
+}[]
+
+export let Services: Type = [
+    { Title: 'Useful Links', name1: 'Home', name2: 'About Us', name3: 'Contact Us', url1: '/', url2: '/Aboutus', url3: '/Contactus' },
+    { Title: 'Our Services', name1: 'Income Certificate', name2: 'Caste Certificate', name3: 'Pan Card', url1: '/Services', url2: '/Services', url3: '/Services' }
+]
