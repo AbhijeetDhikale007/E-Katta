09354d7c36ba009dbefe11315fac49d1279e78f8
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2025-01-02 10:02:32 +0000

    Footer Added

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 import Header from './components/Header/Header.svelte'
 import Wrapper from './components/Wrapper/Wrapper.svelte'
 import Ico from './Icons/Ico.svelte'
+import Footer from './components/Footer/Footer.svelte'
 
-export { Header, Wrapper, Ico }
+export { Header, Wrapper, Ico, Footer }
