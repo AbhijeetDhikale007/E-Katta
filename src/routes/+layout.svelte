291eac3417ffb1cd1049df055b4dc98cb2326ab6
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-03 06:49:14 +0000

    +layout Added

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+    // import type { LayoutData } from './src/routes/$types.js';
+    import { Header, Footer } from '$lib/index.js'
+    import './app.scss'
+    import 'virtual:uno.css'
+
+    let { children }: { children: Snippet } = $props();
+</script>
+
+<!-- {@render children()} -->
+<Header />
+<Footer />
