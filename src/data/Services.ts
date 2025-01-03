52d40dc4abbd9bca84ebb0101a8e463d78de9379
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-03 10:53:19 +0000

    Services Added

--- src/data/Services.ts
+++ src/data/Services.ts
@@ -3,9 +3,9 @@ type Type =
 
 export let Services: Type = [
     { Img: 'Services/Driver-License.png', Alt: 'Driving License', Title: 'Driver License', Info: 'Driver License' },
-    { Img: '', Alt: '', Title: '', Info: '' },
-    { Img: '', Alt: '', Title: '', Info: '' },
-    { Img: '', Alt: '', Title: '', Info: '' },
+    { Img: 'Services/Food-License.png', Alt: 'Food License', Title: 'Food License', Info: 'Food License' },
+    { Img: 'Services/Pan-Card.png', Alt: 'Pan Card', Title: 'Pan Card', Info: 'New Pan Card and Pan Card Correction' },
+    { Img: '', Alt: 'Voter Card', Title: 'Voter Card', Info: 'Voter Card' },
     { Img: '', Alt: '', Title: '', Info: '' },
     { Img: '', Alt: '', Title: '', Info: '' },
     { Img: '', Alt: '', Title: '', Info: '' },
