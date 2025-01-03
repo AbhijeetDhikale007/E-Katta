12e0b2769745be7d418d74aff053db1b4b04b6ab
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-03 07:25:24 +0000

    Services Data Added

---
+++ src/data/Services.ts
@@ -0,0 +1,16 @@
+type Type =
+    { Img: string, Alt: string, Title: string, Info: string }[]
+
+export let Services: Type = [
+    { Img: '', Alt: '', Title: '', Info: '' },
+    { Img: '', Alt: '', Title: '', Info: '' },
+    { Img: '', Alt: '', Title: '', Info: '' },
+    { Img: '', Alt: '', Title: '', Info: '' },
+    { Img: '', Alt: '', Title: '', Info: '' },
+    { Img: '', Alt: '', Title: '', Info: '' },
+    { Img: '', Alt: '', Title: '', Info: '' },
+    { Img: '', Alt: '', Title: '', Info: '' },
+    { Img: '', Alt: '', Title: '', Info: '' },
+    { Img: '', Alt: '', Title: '', Info: '' },
+    { Img: '', Alt: '', Title: '', Info: '' },
+]
