8bdccc4a554adf65f53152370b19c905e53524b7
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-04 13:39:20 +0000

    Text Size Changed

--- src/routes/Aboutus/+page.svelte
+++ src/routes/Aboutus/+page.svelte
@@ -4,9 +4,9 @@
         <div class='flex items-center gap-15'>
             <div class='flex flex-col gap-6 items-center'>
                 <img class='Logo w-50 h-50 rounded-25' src='Ekatta.jpg' alt='Logo'>
-                <p class='Brand text-10'>E-Katta</p>
+                <p class='Brand text-11'>E-Katta</p>
             </div>
-                <p class='text-5 text-justify'>We have done our absolute best to ensure our team is filled with only the most experienced and competent personnel available. That’s why we’re confident that everyone, from our front desk receptionists to our trusted professional tax advisors will be able to help you out every single step of the way. The way that we treat all of our customers has not been overlooked and, as a result, we have recieved a lot of positive feedback. Which, of course, pushes us to be even better.</p>
+            <p class='text-6 text-justify'>We have done our absolute best to ensure our team is filled with only the most experienced and competent personnel available. That’s why we’re confident that everyone, from our front desk receptionists to our trusted professional tax advisors will be able to help you out every single step of the way. The way that we treat all of our customers has not been overlooked and, as a result, we have recieved a lot of positive feedback. Which, of course, pushes us to be even better.</p>
         </div>
     </div>
 </main>
