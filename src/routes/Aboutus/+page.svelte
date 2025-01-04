673304d1e7edf2dc44ba475b319554d067160c49
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-04 07:52:41 +0000

    2 Pages Added

---
+++ src/routes/Aboutus/+page.svelte
@@ -0,0 +1,9 @@
+<script lang='ts'>
+    import { Wrapper } from '$lib/index.js'
+</script>
+
+<Wrapper>
+    <main slot='Wrapper' class='Wrapper'>
+        <p class='absolute top-20% text-center font-700 text-10'>About Us</p>
+    </main>
+</Wrapper>
---
+++ src/routes/Contactus/+page.svelte
@@ -0,0 +1,9 @@
+<script lang='ts'>
+    import { Wrapper } from '$lib/index.js'
+</script>
+
+<Wrapper>
+    <main slot='Wrapper' class='Wrapper'>
+        <p class='absolute top-20% text-center font-700 text-10'>Contact Us</p>
+    </main>
+</Wrapper>
