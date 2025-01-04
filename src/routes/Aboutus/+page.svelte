85d6a9d333519c3dd23203911f7a85f0e3af163a
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2025-01-04 15:40:20 +0000

    Location Added

--- src/routes/Aboutus/+page.svelte
+++ src/routes/Aboutus/+page.svelte
@@ -1,18 +1,21 @@
-<main class='Wrapper'>
-    <p class='absolute top-20% text-center font-700 text-10'>About Us</p>
-    <div class='w-85%'>
-        <div class='flex items-center gap-15'>
-            <div class='flex flex-col gap-6 items-center'>
-                <img class='Logo w-50 h-50 rounded-25' src='Ekatta.jpg' alt='Logo'>
-                <p class='Brand text-11'>E-Katta</p>
+<main class='flex flex-col items-center py-14'>
+    <div class='Wrapper'>
+        <p class='absolute top-30% text-center font-700 text-10'>About Us</p>
+        <div class='w-85%'>
+            <div class='flex items-center gap-15'>
+                <div class='flex flex-col gap-6 items-center'>
+                    <img class='Logo w-50 h-50 rounded-25' src='Ekatta.jpg' alt='Logo'>
+                    <p class='Brand text-11'>E-Katta</p>
+                </div>
+                <p class='text-6 text-justify'>We have done our absolute best to ensure our team is filled with only the most experienced and competent personnel available. That’s why we’re confident that everyone, from our front desk receptionists to our trusted professional tax advisors will be able to help you out every single step of the way. The way that we treat all of our customers has not been overlooked and, as a result, we have recieved a lot of positive feedback. Which, of course, pushes us to be even better.</p>
             </div>
-            <p class='text-6 text-justify'>We have done our absolute best to ensure our team is filled with only the most experienced and competent personnel available. That’s why we’re confident that everyone, from our front desk receptionists to our trusted professional tax advisors will be able to help you out every single step of the way. The way that we treat all of our customers has not been overlooked and, as a result, we have recieved a lot of positive feedback. Which, of course, pushes us to be even better.</p>
         </div>
     </div>
+    <iframe class='w-85% rounded-2' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d185.16127931147838!2d73.8653403!3d20.0348556!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bddc1ed1366861f%3A0xcc95e45a0be624d7!2sE-Katta%20Online%20Center!5e1!3m2!1sen!2sin!4v1736004598607!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </main>
 
 <style lang='scss'>
-    .Logo, .Brand {
+    .Logo, .Brand, iframe {
         filter: drop-shadow(0 0 10px #000);
     }
 </style>
