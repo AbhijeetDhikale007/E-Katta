9fc1fca7e068bc77511ef4db1e41f5cad52523b3
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2025-01-04 15:47:57 +0000

    Location Added In Aboutus

--- src/routes/Aboutus/+page.svelte
+++ src/routes/Aboutus/+page.svelte
@@ -1,3 +1,7 @@
+<script lang='ts'>
+    import { Location } from '$lib/index.js'
+</script>
+
 <main class='flex flex-col items-center py-14'>
     <div class='Wrapper'>
         <p class='absolute top-30% text-center font-700 text-10'>About Us</p>
@@ -11,11 +15,11 @@
             </div>
         </div>
     </div>
-    <iframe class='w-85% rounded-4' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d185.16127931147838!2d73.8653403!3d20.0348556!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bddc1ed1366861f%3A0xcc95e45a0be624d7!2sE-Katta%20Online%20Center!5e1!3m2!1sen!2sin!4v1736004598607!5m2!1sen!2sin" width="600" height="450" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title='Location of E-Katta Online Center'></iframe>
+    <Location />
 </main>
 
 <style lang='scss'>
-    .Logo, .Brand, iframe {
+    .Logo, .Brand {
         filter: drop-shadow(0 0 10px #000);
     }
 </style>
