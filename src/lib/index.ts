c4b0cc477896505b4bd1ddbae02234fd58e07e3c
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2025-01-04 15:46:11 +0000

    Location Component Included

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 import Header from './components/Header/Header.svelte'
 import Ico from './Icons/Ico.svelte'
+import Location from './components/Location/Location.svelte'
 import Footer from './components/Footer/Footer.svelte'
 
-export { Header, Ico, Footer }
+export { Header, Ico, Location, Footer }
