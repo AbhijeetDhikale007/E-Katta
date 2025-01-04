10a4cd917caaa3f25bf5849ab9eab53ebce2a493
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2025-01-04 15:42:07 +0000

    Location Iframe Tag Added

--- src/routes/Aboutus/+page.svelte
+++ src/routes/Aboutus/+page.svelte
@@ -11,7 +11,7 @@
             </div>
         </div>
     </div>
-    <iframe class='w-85% rounded-2' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d185.16127931147838!2d73.8653403!3d20.0348556!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bddc1ed1366861f%3A0xcc95e45a0be624d7!2sE-Katta%20Online%20Center!5e1!3m2!1sen!2sin!4v1736004598607!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe class='w-85% rounded-2' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d185.16127931147838!2d73.8653403!3d20.0348556!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bddc1ed1366861f%3A0xcc95e45a0be624d7!2sE-Katta%20Online%20Center!5e1!3m2!1sen!2sin!4v1736004598607!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title='Location of E-Katta Online Center'></iframe>
 </main>
 
 <style lang='scss'>
