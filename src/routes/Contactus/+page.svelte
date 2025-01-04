c31ba70dd86c5a6d5e99de344152d4750a83a797
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-04 13:58:39 +0000

    Contact Us Page Content Added

--- src/routes/Contactus/+page.svelte
+++ src/routes/Contactus/+page.svelte
@@ -4,18 +4,34 @@
 
 <main class='Wrapper'>
     <p class='absolute top-20% text-center font-700 text-10'>Contact Us</p>
-    <div>
-        <a href='tel:+917040218626' class='flex gap-1 ml-25 color-white no-underline'>
-            <Ico class='w-5' name='Phone' />
-            +91 70402 18626
+    <div class='Aboutus flex gap-14'>
+        <a href='tel:+917040218626' class='flex gap-1 ml-25 color-black no-underline'>
+            <Ico class='w-20' name='Phone' />
+            Phone
         </a>
-        <a href='wa.me/+917040218626' class='flex gap-1 color-white no-underline'>
-            <Ico class='w-5' name='WhatsApp' />
-            +91 70402 18626
+        <a href='wa.me/+917040218626' class='flex gap-1 color-black no-underline'>
+            <Ico class='w-20 invert' name='WhatsApp' />
+            WhatsApp
         </a>
-        <a href='emailto:ekatta.online@gmail.com' class='flex gap-1 color-white no-underline'>
-            <Ico class='w-5' name='Mail' />
-            ekatta.online@gmail.com
+        <a href='emailto:ekatta.online@gmail.com' class='flex gap-1 color-black no-underline'>
+            <Ico class='w-20' name='Mail' />
+            Email
         </a>
     </div>
 </main>
+
+<style lang='scss'>
+    .Aboutus {
+        a {
+            border: 1px solid orangered;
+            transition: .5s ease;
+            box-shadow: 0 0 10px rgb(0, 0, 0, .6);
+            filter: drop-shadow(0 0 10px rgb(0, 0, 0, .2));
+            @apply flex flex-col items-center justify-center p-4 w-30 h-30 rounded-5;
+        }
+
+        a:hover {
+            scale: 1.1;
+        }
+    }
+</style>
