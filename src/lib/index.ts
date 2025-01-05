dc0956182e80cc13f9f08fddd2266c0d20a29db1
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-05 12:07:52 +0000

    Heading Component Added

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 import Header from './components/Header/Header.svelte'
 import Ico from './Icons/Ico.svelte'
+import Heading from './components/Heading/Heading.svelte'
 import Location from './components/Location/Location.svelte'
 import Footer from './components/Footer/Footer.svelte'
 
-export { Header, Ico, Location, Footer }
+export { Header, Ico, Heading, Location, Footer }
