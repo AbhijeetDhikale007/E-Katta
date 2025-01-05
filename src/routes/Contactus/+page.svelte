8a029fb8cff2f72c1d25501792a267787f69c75b
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-05 08:44:34 +0000

    Code Simplified

--- src/routes/Contactus/+page.svelte
+++ src/routes/Contactus/+page.svelte
@@ -2,27 +2,25 @@
     import { Ico } from '$lib/index.js'
 </script>
 
-<!-- <main class='Wrapper'> -->
-    <p class='absolute top-20% text-center font-700 text-10'>Contact Us</p>
-    <div class='Aboutus flex gap-14'>
-        <a href='tel:+917040218626' class='flex gap-1 color-black no-underline' target='_blank'>
-            <Ico class='w-20' name='Phone' />
-            Phone
-        </a>
-        <a href='https://wa.me/+917040218626' class='flex gap-1 color-black no-underline' target='_blank'>
-            <Ico class='w-20 invert' name='WhatsApp' />
-            WhatsApp
-        </a>
-        <a href='https://emailto:ekatta.online@gmail.com' class='flex gap-1 color-black no-underline' target='_blank'>
-            <Ico class='w-20' name='Mail' />
-            Email
-        </a>
-        <a href='https://t.me/labhadesanket' class='flex gap-1 color-black no-underline' target='_blank'>
-            <Ico class='w-20' name='Telegram' />
-            Telegram
-        </a>
-    </div>
-<!-- </main> -->
+<p class='absolute top-20% text-center font-700 text-10'>Contact Us</p>
+<div class='Aboutus flex gap-14'>
+    <a href='tel:+917040218626' class='flex gap-1 color-black no-underline' target='_blank'>
+        <Ico class='w-20' name='Phone' />
+        Phone
+    </a>
+    <a href='https://wa.me/+917040218626' class='flex gap-1 color-black no-underline' target='_blank'>
+        <Ico class='w-20 invert' name='WhatsApp' />
+        WhatsApp
+    </a>
+    <a href='https://emailto:ekatta.online@gmail.com' class='flex gap-1 color-black no-underline' target='_blank'>
+        <Ico class='w-20' name='Mail' />
+        Email
+    </a>
+    <a href='https://t.me/labhadesanket' class='flex gap-1 color-black no-underline' target='_blank'>
+        <Ico class='w-20' name='Telegram' />
+        Telegram
+    </a>
+</div>
 
 <style lang='scss'>
     .Aboutus {
