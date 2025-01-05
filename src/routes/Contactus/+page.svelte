f02f2bcfa2ae89456596996f08475bf7a478402e
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-05 08:41:25 +0000

    Target Blank Added To Links

--- src/routes/Contactus/+page.svelte
+++ src/routes/Contactus/+page.svelte
@@ -5,19 +5,19 @@
 <main class='Wrapper'>
     <p class='absolute top-20% text-center font-700 text-10'>Contact Us</p>
     <div class='Aboutus flex gap-14'>
-        <a href='tel:+917040218626' class='flex gap-1 color-black no-underline'>
+        <a href='tel:+917040218626' class='flex gap-1 color-black no-underline' target='_blank'>
             <Ico class='w-20' name='Phone' />
             Phone
         </a>
-        <a href='https://wa.me/+917040218626' class='flex gap-1 color-black no-underline'>
+        <a href='https://wa.me/+917040218626' class='flex gap-1 color-black no-underline' target='_blank'>
             <Ico class='w-20 invert' name='WhatsApp' />
             WhatsApp
         </a>
-        <a href='https://emailto:ekatta.online@gmail.com' class='flex gap-1 color-black no-underline'>
+        <a href='https://emailto:ekatta.online@gmail.com' class='flex gap-1 color-black no-underline' target='_blank'>
             <Ico class='w-20' name='Mail' />
             Email
         </a>
-        <a href='https://t.me/labhadesanket' class='flex gap-1 color-black no-underline'>
+        <a href='https://t.me/labhadesanket' class='flex gap-1 color-black no-underline' target='_blank'>
             <Ico class='w-20' name='Telegram' />
             Telegram
         </a>
