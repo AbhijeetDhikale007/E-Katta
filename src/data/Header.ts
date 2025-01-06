fc352ecb2182cc75d2c6d0542d764c73c04e4f02
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-06 12:56:49 +0000

    Done

---
+++ src/data/Header.ts
@@ -0,0 +1,7 @@
+type Type1 = [
+    { Numb: string, NumbLink: string, WhatsAppNum: string, WhatsAppLink: string, Email: string }
+]
+
+type Type2 = [
+    { Logo: string, Title: string, Logo1?: string, Logo2?: string, Logo3?: string }
+]
