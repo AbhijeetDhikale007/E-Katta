b9769ce444dc6489f442deaadb3b65290af1a6f5
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2025-01-06 16:32:25 +0000

    Marathi Implimented

--- src/data/Header.ts
+++ src/data/Header.ts
@@ -1,7 +1,19 @@
 type Type1 = [
-    { Numb: string, NumbLink: string, WhatsAppNum: string, WhatsAppLink: string, Email: string }
+    { Numb: string, NumbLink: string, WhatsAppNum: string, WhatsAppLink: string, Email: string, EmailLink: string, Lang1: string, Lang2: string, Lang3?: string, Lang4?: string }
 ]
 
 type Type2 = [
-    { Logo: string, Title: string, Logo1?: string, Logo2?: string, Logo3?: string }
+    { Logo: string, Title: string, Logo1?: string, Logo2?: string, Logo3?: string, Button1?: string, Button1Link?: string, Button2?: string,  Button2Link?: string, Button3?: string, Button3Link?: string, Button4?: string, Button4Link?: string }
+]
+
+export let Header1: Type1 = [
+    { Numb: '+91 70402 18626', NumbLink: 'tel:+917040218626', WhatsAppNum: '+91 70402 18626', WhatsAppLink: 'https://wa.me/+917040218626', Email: 'ekatta.online@gmail.com', EmailLink: 'emailto:ekatta.online@gmail.com', Lang1: 'English', Lang2: 'मराठी' }
+]
+
+export let Header2Eng: Type2 = [
+    { Logo: 'Ekatta.jpg', Title: 'E-Katta', Logo1: 'Digital-India.png', Logo2: 'My-Gov.png', Button1: 'Home', Button1Link: '/', Button2: 'Services',  Button2Link: '/Services', Button3: 'About Us', Button3Link: '/Aboutus', Button4: 'Contact Us', Button4Link: '/Contactus' }
+]
+
+export let Header2Mar: Type2 = [
+    { Logo: 'Ekatta.jpg', Title: 'ई-कट्टा', Logo1: 'Digital-India.png', Logo2: 'My-Gov.png', Button1: 'होम', Button1Link: '/', Button2: 'सर्विसेस',  Button2Link: '/Services', Button3: 'आमच्या बद्दल', Button3Link: '/Aboutus', Button4: 'आमचा संपर्क', Button4Link: '/Contactus' }
 ]
--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -1,48 +1,55 @@
 <script lang='ts'>
     import { Ico } from '$lib/index.js'
     import { Lang } from '$store'
+    import { Header1, Header2Eng, Header2Mar } from '$data/Header.js'
 
-    $Lang = 'Eng'
     function LangButton(Name: string) {
         $Lang = Name
     }
+
+    let Header2: any
+    $: Header2 = $Lang === 'Eng' ? Header2Eng : Header2Mar;
 </script>
 
 <header class='flex flex-col place-items-center relative top-0 h-28 w-100%'>
+    {#each Header1 as header1}
     <div class='flex justify-between items-center h-30% w-100% bg-red-600'>
         <div class='flex gap-10'>
-            <a href='tel:+917040218626' class='flex gap-1 ml-25 color-white no-underline' target='_blank'>
+            <a href={header1.NumbLink} class='flex gap-1 ml-25 color-white no-underline' target='_blank'>
                 <Ico class='w-5' name='Phone' />
-                +91 70402 18626
+                {header1.Numb}
             </a>
-            <a href='https://wa.me/+917040218626' class='flex gap-1 color-white no-underline' target='_blank'>
+            <a href={header1.WhatsAppLink} class='flex gap-1 color-white no-underline' target='_blank'>
                 <Ico class='w-5' name='WhatsApp' />
-                +91 70402 18626
+                {header1.WhatsAppNum}
             </a>
-            <a href='emailto:ekatta.online@gmail.com' class='flex gap-1 color-white no-underline' target='_blank'>
+            <a href={header1.EmailLink} class='flex gap-1 color-white no-underline' target='_blank'>
                 <Ico class='w-5' name='Mail' />
-                ekatta.online@gmail.com
+                {header1.Email}
             </a>    
         </div>
         <div class='flex gap-6 mr-25 text-3.6'>
-            <button class='px-2 py-1 border-none rounded-1 bg-transparent hover:bg-white hover:color-black' on:click={() => LangButton('Eng')}>English</button>
-            <button class='px-2 py-1 border-none rounded-1 bg-transparent hover:bg-white hover:color-black' on:click={() => LangButton('Mar')}>Marathi</button>
+            <button class='px-2 py-1 border-none rounded-1 bg-transparent hover:bg-white hover:color-black' on:click={() => LangButton('Eng')}>{header1.Lang1}</button>
+            <button class='px-2 py-1 border-none rounded-1 bg-transparent hover:bg-white hover:color-black' on:click={() => LangButton('Mar')}>{header1.Lang2}</button>
         </div>
     </div>
+    {/each}
+    {#each Header2 as header2}
     <div class='flex items-center h-70% w-100% justify-between'>
         <div class='ml-48 h-100% gap-4 w-fit flex items-center'>
-            <img class='Logo h-15 w-15 rounded-12' src='Ekatta.jpg' alt='Logo'>
-            <h2 class=''>E-Katta</h2>
-            <img class='h-8 w-12' src='Digital-India.png' alt='Logo'>
-            <img class='h-8 w-12' src='My-Gov.png' alt='Logo'>
+            <img class='Logo h-15 w-15 rounded-12' src={header2.Logo} alt='Logo1'>
+            <h2 class=''>{header2.Title}</h2>
+            <img class='h-8 w-12' src={header2.Logo1} alt='Logo2'>
+            <img class='h-8 w-12' src={header2.Logo2} alt='Logo3'>
         </div>
         <nav class='mr-48'>
-            <a class='Button' href="/">Home</a>
-            <a class='Button' href="/Services">Services</a>
-            <a class='Button' href="/Aboutus">About Us</a>
-            <a class='Button' href="/Contactus">Contact Us</a>
+            <a class='Button' href={header2.Button1Link}>{header2.Button1}</a>
+            <a class='Button' href={header2.Button2Link}>{header2.Button2}</a>
+            <a class='Button' href={header2.Button3Link}>{header2.Button3}</a>
+            <a class='Button' href={header2.Button4Link}>{header2.Button4}</a>
         </nav>
     </div>
+    {/each}
 </header>
 
 <style>
