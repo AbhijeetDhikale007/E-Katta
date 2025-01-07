043cd2900315aa8ffc6cebf14fabeaa7d266e1fa
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-07 05:28:07 +0000

    Heading Data Added With 2 Languages

---
+++ src/data/Heading.ts
@@ -0,0 +1,11 @@
+type Type = [
+    {Heading1: string, Heading1Name: string, Heading2: string, Heading3: string, Heading4?: string, Heading5?: string, Heading6?: string}
+]
+
+export let HeadingEng: Type = [
+    {Heading1: 'Welcome', Heading1Name: 'Services We Provide:', Heading2: 'Services', Heading3: 'About Us', Heading4: 'Contact Us' }
+]
+
+export let HeadingMar: Type = [
+    {Heading1: 'सुस्वागतम', Heading1Name: 'आम्ही पुरवतो त्या सेवा:', Heading2: 'सर्विसिस', Heading3: 'आमच्या बद्दल', Heading4: 'आमचा संपर्क' }
+]
