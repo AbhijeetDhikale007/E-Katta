42f0c532562f9bfa79d1604f8e8e8a2cdb3d38da
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-08 05:42:56 +0000

    Location Added

--- src/routes/Contactus/+page.svelte
+++ src/routes/Contactus/+page.svelte
@@ -2,32 +2,36 @@
     import { Lang } from '$store'
     import { HeadingEng, HeadingMar } from '$data/Heading.js';
     import { Ico, Heading } from '$lib/index.js'
+    import { Location } from '$lib/index.js'
 
     let HeadingContact: any
     $: HeadingContact = $Lang === 'Eng' ? HeadingEng : HeadingMar;
 </script>
 
 {#each HeadingContact as heading}
-<div class='flex self-start flex-col h-[40vh] gap-20'>
+<div class='flex self-start flex-col h-fit gap-20'>
     <Heading Title={heading.Heading4} />
-<div class='Aboutus flex gap-14'>
-    <a href='tel:+917040218626' class='flex gap-1 color-black no-underline select-none' target='_blank'>
-        <Ico class='w-20 Ico' name='Call' />
-        Phone
-    </a>
-    <a href='https://wa.me/+917040218626' class='flex gap-1 color-black no-underline select-none' target='_blank'>
-        <Ico class='w-20 Ico' name='WhatsAppColor' />
-        WhatsApp
-    </a>
-    <a href='emailto:ekatta.online@gmail.com' class='flex gap-1 color-black no-underline select-none' target='_blank'>
-        <Ico class='w-20 Ico' name='GMail' />
-        Email
-    </a>
-    <a href='https://t.me/labhadesanket' class='flex gap-1 color-black no-underline select-none' target='_blank'>
-        <Ico class='w-20 Ico' name='Telegram' />
-        Telegram
-    </a>
-</div>
+    <div class='Aboutus justify-center flex gap-14'>
+        <a href='tel:+917040218626' class='flex gap-1 color-black no-underline select-none' target='_blank'>
+            <Ico class='w-20 Ico' name='Call' />
+            Phone
+        </a>
+        <a href='https://wa.me/+917040218626' class='flex gap-1 color-black no-underline select-none' target='_blank'>
+            <Ico class='w-20 Ico' name='WhatsAppColor' />
+            WhatsApp
+        </a>
+        <a href='emailto:ekatta.online@gmail.com' class='flex gap-1 color-black no-underline select-none' target='_blank'>
+            <Ico class='w-20 Ico' name='GMail' />
+            Email
+        </a>
+        <a href='https://t.me/labhadesanket' class='flex gap-1 color-black no-underline select-none' target='_blank'>
+            <Ico class='w-20 Ico' name='Telegram' />
+            Telegram
+        </a>
+    </div>
+    <div class='self-end'>
+        <Location />
+    </div>
 </div>
 {/each}
 
