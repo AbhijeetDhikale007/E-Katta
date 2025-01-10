79b860858bcff1617a29423517d6f7c56a0e2989
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-10 13:39:42 +0000

    Removed: Bg Video

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,10 +8,6 @@
     let { children }: { children: Snippet } = $props();
 </script>
 
-<video class='hidden absolute md:flex scale-115 fixed bg-cover left-0 top-0 w-screen h-screen z--1' autoplay muted>
-    <source src="Background.mp4" type="video/mp4">
-</video>
-
 <Header />
 <main class='Wrapper'>
     {@render children()}
