b258a961c056521a3cf7657983b9826b6f172f74
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-16 12:27:13 +0000

    Added Function For Routing On Button

--- src/routes/Services/+page.svelte
+++ src/routes/Services/+page.svelte
@@ -1,6 +1,6 @@
 <script lang='ts'>
     import { Heading } from '$lib/index.js'
-    import { Lang } from '$store'
+    import { Lang, ServicesPage } from '$store'
     import { HeadingEng, HeadingMar } from '$data/Heading.js'
     import { ServicesEng, ServicesMar } from '$data/Services.js'
 
@@ -8,20 +8,25 @@
     let ServicesHeading: any
     $: Services = $Lang === 'Eng' ? ServicesEng : ServicesMar;
     $: ServicesHeading = $Lang === 'Eng' ? HeadingEng : HeadingMar;
+
+    $ServicesPage = 'Income'
+    function ServicePageRouting(Page: string) {
+        $ServicesPage = Page
+    }
 </script>
 
 {#each ServicesHeading as heading}
 <Heading Title={heading.Heading2} />
 <main class='Wrapper'>
-    <div class='flex flex-wrap items-center justify-center gap-5 md:gap-12 lg:gap-18 py-10 w-100% md:w-90% min-h-100'>
+    <div class='flex flex-wrap items-center justify-center gap-8 md:gap-16 lg:gap-22 py-10 w-100% md:w-90% min-h-100'>
         {#each Services as services}
-        <div class='ServiceCard flex flex-col p-2 md:p-4 gap-3 md:gap-5 w-32% min-h-[5vh] md:w-20% md:min-h-[38vh] text-white justify-center items-center rounded-2 md:rounded-4'>
+        <button class='ServiceCard flex flex-col border-none p-2 md:p-4 gap-3 md:gap-5 w-32% min-h-[5vh] md:w-20% md:min-h-[38vh] text-white justify-center items-center rounded-2 md:rounded-4' on:click={() => ServicePageRouting(services.Page)}>
             <img class='w-94% md:min-h-20vh rounded-2' src={services.Img} alt={services.Alt}>
             <div class='text-center min-h-5vh md:min-h-10vh w-90%'>
                 <p class='md:text-5 lg:text-8'>{services.Title}</p>
                 <!-- <p>{services.Info}</p> -->
             </div>
-        </div>
+        </button>
         {/each}
     </div>
 </main>
