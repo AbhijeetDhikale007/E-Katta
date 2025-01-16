4b54bfedfe4e3db9fc07b0567b5b111e46c89770
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-16 13:15:39 +0000

    Added: ServicesPage

---
+++ src/routes/ServicesPage/+page.svelte
@@ -0,0 +1,10 @@
+<script lang='ts'>
+    import { ServicesPage } from '$data/ServicesPage.js'
+	import Services from '$lib/components/Footer/Services.svelte';
+</script>
+
+<!-- {#each ServicesPage.Income as Services}
+<div>
+    <h1>{Services.name}</h1>
+</div>
+{/each} -->
