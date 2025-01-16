d0ea7cc3b0b8f68a2cd4f8b1dc58610056af19f7
Λʙʜιנєєτ Ðʜικαℓє <150115341+AbhijeetDhikale007@users.noreply.github.com>
2025-01-16 12:19:24 +0000

    Created: ServicesPage Data

---
+++ src/data/ServicesPage.ts
@@ -0,0 +1,234 @@
+type Type = {
+    [key: string]: {
+        name: string,
+        R1: string,
+        R2?: string,
+        R3?:string,
+        R4?: string,
+        R5?: string,
+        R6?: string,
+        R7?: string,
+        R8?: string
+    }
+}
+
+export let ServicesPage: Type = {
+    Income: {
+        name: '0 0 32 32',
+        R1: 'Aadhaar Card'
+    },
+
+    GST: {
+        name: 'GST Registration and Return',
+        R1: 'Aadhaar Card'
+    },
+
+    AnnaSaheb: {
+        name: 'AnnaSaheb Patil Yojna',
+        R1: 'Aadhaar Card',
+        R2: '',
+        R3: '',
+        R4: '',
+        R5: '',
+        R6: '',
+        R7: ''
+    },
+
+    VehicleInsurence: {
+        name: 'Vehicle Insurence',
+        R1: 'Aadhaar Card',
+        R2: '',
+        R3: '',
+        R4: '',
+        R5: '',
+        R6: '',
+        R7: ''
+    },
+
+    ShopAct: {
+        name: 'Shop Act Registration',
+        R1: 'Aadhaar Card',
+        R2: '',
+        R3: '',
+        R4: '',
+        R5: '',
+        R6: '',
+        R7: ''
+    },
+
+    Udyog: {
+        name: 'Udyog Aadhaar',
+        R1: 'Aadhaar Card',
+        R2: '',
+        R3: '',
+        R4: '',
+        R5: '',
+        R6: '',
+        R7: ''
+    },
+
+    Scholorship: {
+        name: 'Scholorship Form',
+        R1: 'Aadhaar Card',
+        R2: '',
+        R3: '',
+        R4: '',
+        R5: '',
+        R6: '',
+        R7: ''
+    },
+
+    Domicile: {
+        name: 'Domicile Certificate',
+        R1: 'Aadhaar Card',
+        R2: '',
+        R3: '',
+        R4: '',
+        R5: '',
+        R6: '',
+        R7: ''
+    },
+
+    Caste: {
+        name: 'Caste Certificate',
+        R1: 'Aadhaar Card',
+        R2: '',
+        R3: '',
+        R4: '',
+        R5: '',
+        R6: '',
+        R7: ''
+    },
+
+    Validity: {
+        name: 'Caste Validity',
+        R1: 'Aadhaar Card',
+        R2: '',
+        R3: '',
+        R4: '',
+        R5: '',
+        R6: '',
+        R7: ''
+    },
+
+    Police: {
+        name: 'Police Verification',
+        R1: 'Aadhaar Card',
+        R2: '',
+        R3: '',
+        R4: '',
+        R5: '',
+        R6: '',
+        R7: ''
+    },
+
+    DrivingLicense: {
+        name: 'Driving License',
+        R1: 'Aadhaar Card',
+        R2: '',
+        R3: '',
+        R4: '',
+        R5: '',
+        R6: '',
+        R7: ''
+    },
+
+    FoodLicense: {
+        name: 'Food License',
+        R1: 'Aadhaar Card',
+        R2: '',
+        R3: '',
+        R4: '',
+        R5: '',
+        R6: '',
+        R7: ''
+    },
+
+    Pan: {
+        name: 'Pan Card',
+        R1: 'Aadhaar Card',
+        R2: '',
+        R3: '',
+        R4: '',
+        R5: '',
+        R6: '',
+        R7: ''
+    },
+
+    Voter: {
+        name: 'Voter Card',
+        R1: 'Aadhaar Card',
+        R2: '',
+        R3: '',
+        R4: '',
+        R5: '',
+        R6: '',
+        R7: ''
+    },
+
+    HomeInsurence: {
+        name: 'Home Insurence',
+        R1: 'Aadhaar Card',
+        R2: '',
+        R3: '',
+        R4: '',
+        R5: '',
+        R6: '',
+        R7: ''
+    },
+
+    HealthInsurence: {
+        name: 'Health Insurence',
+        R1: 'Aadhaar Card',
+        R2: '',
+        R3: '',
+        R4: '',
+        R5: '',
+        R6: '',
+        R7: ''
+    },
+
+    PF: {
+        name: 'PF Withraw',
+        R1: 'Aadhaar Card',
+        R2: '',
+        R3: '',
+        R4: '',
+        R5: '',
+        R6: '',
+        R7: ''
+    },
+
+    OnlineForms: {
+        name: 'Online Forms',
+        R1: 'Aadhaar Card',
+        R2: '',
+        R3: '',
+        R4: '',
+        R5: '',
+        R6: '',
+        R7: ''
+    },
+
+    Passport: {
+        name: 'Passport',
+        R1: 'Aadhaar Card',
+        R2: '',
+        R3: '',
+        R4: '',
+        R5: '',
+        R6: '',
+        R7: ''
+    },
+
+    EGazette: {
+        name: 'E Gazette',
+        R1: 'Aadhaar Card',
+        R2: '',
+        R3: '',
+        R4: '',
+        R5: '',
+        R6: '',
+        R7: ''
+    },
+}
